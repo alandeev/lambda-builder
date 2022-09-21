122f416c76c05a428f8add431e4c6e4b1b2d5f31
alandev2 <alanworking1@gmail.com>
2022-09-21 07:55:14 -0300

    test: ls archiver zip

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 const { readdirSync } = require('fs')
+
 const buildESBuildLambda = require('./builders/esbuild')
 const getFiles = require('./utils/get-files')
 const parseHandlersFiles = require('./utils/parse-handlers-files')
 const parseHandlersYML = require('./utils/parse-handlers-yml')
 const zipDirectory = require('./utils/zip-directory')
 
+const sleep = ms => new Promise(resolve => setTimeout(resolve, ms))
+
 const builders = {
-  esbuild: buildESBuildLambda
+  esbuild: buildESBuildLambda,
 }
 
 /**
@@ -18,10 +21,12 @@ const builders = {
  *  sourcemap?: boolean,
  *  bundle?: boolean,
  *  individually?: boolean,
- *  zip?: boolean
+ *  zip?: boolean,
+ *  three_shaking?: boolean,
+ *  minify?: boolean
  * }} BuildOptions
- * 
- * @param {BuildOptions} options 
+ *
+ * @param {BuildOptions} options
  */
 const buildRun = async (options = {}) => {
   const buildOptions = {
@@ -32,17 +37,19 @@ const buildRun = async (options = {}) => {
     sourcemap: options.sourcemap ?? true,
     bundle: options.bundle ?? true,
     individually: options.individually ?? true,
-    zip: options.zip ?? true
+    zip: options.zip ?? true,
+    three_shaking: options.three_shaking ?? false,
+    minify: options.minify ?? false
   }
 
   const builder = builders[buildOptions.builder ?? 'esbuild']
 
-  if(buildOptions.individually) {
+  if (buildOptions.individually) {
     const handlers = parseHandlersYML(buildOptions.functions)
 
     await builder({
       handlers,
-      build: buildOptions
+      build: buildOptions,
     })
   } else {
     const files = await getFiles(buildOptions.base_dir)
@@ -50,25 +57,34 @@ const buildRun = async (options = {}) => {
 
     await builder({
       handlers,
-      build: buildOptions
+      build: buildOptions,
     })
   }
 
-  if(buildOptions.zip) {
+  if (buildOptions.zip) {
     const handlers = readdirSync(buildOptions.outdir)
     console.debug(`Starting lambdas compress: - ${handlers.length} function(s)`)
 
-    for(let handler of handlers) {
+    const promises = []
+    for (const handler of handlers) {
       const inputFolder = `${process.cwd()}/${buildOptions.outdir}/${handler}`
       const outZip = `${buildOptions.outdir}/${handler}.zip`
-      
-      console.time(`Compress lambda - ${outZip}`)
-      await zipDirectory(inputFolder, outZip)
-      console.timeEnd(`Compress lambda - ${outZip}`)
+
+      const promiseResult = zipDirectory(inputFolder, outZip)
+      promises.push(promiseResult)
+
+      await sleep(200)
     }
+
+    await Promise.all(promises)
   }
 
-  console.info(`Build finished`)
+  console.info('Build finished')
 }
 
-buildRun({})
+buildRun({
+  individually: true,
+  three_shaking: true,
+  sourcemap: true,
+  minify: true,
+})
